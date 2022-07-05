d7c26302aea07181587350fe643455a6041e8beb
Fernweh99 <daniele.dimento@gmail.com>
2022-07-05 14:18:28 +0200

    finish milestone 0

--- js/script.js
+++ js/script.js
@@ -12,3 +12,38 @@ Trasformare la stringa foto in una immagine effettiva (le trovate in allegato)
 # BONUS 2:
 Organizzare i singoli membri in card/schede e ispirarci alle grafiche in allegato
 */
+
+const teamPersons = [
+  {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg',
+  },
+  {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
+  },
+  {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
+  },
+  {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg',
+  },
+  {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
+  }
+];
+
+console.log(teamPersons);
